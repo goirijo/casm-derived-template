8d1b390d37fe624d032924ce93d1929a73769f44
John Goiri <jg.goiri@gmail.com>
2016-09-08 16:05:51 -0700

    All set up?

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <casm/CASM_global_definitions.hh>
+
+int main()
+{
+    /**
+     * This is where you get to shine. Place all your code in here
+     * and use the infrastructure of the repository to compile
+     * and install.
+     *
+     * If you're interested in working with something more complicated
+     * than a single main.cpp file is convenient for, check out
+     * casm-utilities!
+     * https://github.com/goirijo/casm-utilities
+     */
+
+    std::cout<<"I am a sad sad executable incapable of anything"<<std::endl;
+    std::cout<<"Here is the CASM logo:"<<std::endl;
+
+
+    return 0;
+}
