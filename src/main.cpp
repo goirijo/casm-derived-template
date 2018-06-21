dfc8ac18fe55098041e46840572c6a231b705baf
Jonas <jonas.l.kaufman@gmail.com>
2018-06-21 13:02:08 -0700

    find prim implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <casm/CASM_global_definitions.hh>
 #include <casm/crystallography/Structure.hh>
+#include <casm/crystallography/Niggli.hh>
 #include <casm/casm_io/VaspIO.hh>
 
 int main()
 {
     /**
-     * Let's read and write a POSCAR file
+     * Let's read a POSCAR file, primitize it, and write it
      */
 
 	// read
 	std::string infile = "POSCAR";
 	CASM::Structure input(infile);
+
+	// check primitive
+	CASM::BasicStructure<CASM::Site> true_prim;
+	true_prim.title = input.title;
+    	if(!input.is_primitive(true_prim)) {
+		std::cout << "Structure is not primitive, finding prim" << std::endl;
+        	CASM::Structure tmp(true_prim);
+        	CASM::Lattice lat_niggli = canonical_equivalent_lattice(true_prim.lattice(), tmp.point_group(), CASM::TOL);
+        	tmp.set_lattice(lat_niggli, CASM::CART);
+        	input = tmp;
+	} else {
+		std::cout << "Structure is primitive" << std::endl;
+	}
 
 	// write
 	std::string outfile = "POSCAR_out";
